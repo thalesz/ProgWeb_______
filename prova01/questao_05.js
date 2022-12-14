62c4f0b14d7f8982d6a62f8ff1cb845d2d02e96b
thalesz <80847779+thalesz@users.noreply.github.com>
2022-12-14 16:53:33 -0400

    prova01

---
+++ prova01/questao_05.js
@@ -0,0 +1,38 @@
+function vendas(id, qtde, preco){
+    var vendas = {
+        id : 0,
+        qtde : 0,
+        preco : 0,
+        getId: function(){
+            return this.id
+        },
+        setId: function(){
+            this.id=id
+        },
+        getQtde: function(){
+            return this.qtde
+        },
+        setQtde: function(){
+            this.qtde=qtde
+        },
+        getPreco: function(){
+            return this.preco
+        },
+        setId: function(){
+            this.preco=preco
+        },
+        getValorTotal: function(){
+            console.log(this.preco*this.qtde)
+        }
+
+    }
+}
+
+(
+    function(){
+        v1 = new vendas(1,2,30)
+        v1.getValorTotal
+        v2 = vendas(2, 3, 60)
+        v2.getValorTotal
+    }
+)
